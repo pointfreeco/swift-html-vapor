da94d5b3caa0b0e68947006a4026f879670dfa6a
Stephen Celis <stephen@stephencelis.com>
2019-03-26 11:18:14 -0400

    Interpolate

--- Sources/HtmlVaporSupportExample/main.swift
+++ Sources/HtmlVaporSupportExample/main.swift
@@ -88,7 +88,7 @@ immediately, simply do:
       ),
       .ul(
         .li(.code("swift run HtmlVaporSupportExample")),
-        .li("Open your browser to ", .code("http://localhost:8080"))
+        .li("Open your browser to \(.code("http://localhost:8080"))")
       ),
       .p("""
 The HTML for that page is constructed and rendered with swift-html!
@@ -102,9 +102,9 @@ If you want to run the app in Xcode so that you can play around with the HTML, t
         .li(.code("git clone https://github.com/pointfreeco/swift-html-vapor")),
         .li(.code("cd swift-html-vapor")),
         .li(.code("make xcodeproj")),
-        .li("Select the ", .code("HtmlVaporSupportExample"), " target"),
-        .li("Build and run ", .code("cmd+R")),
-        .li("Open your browser to ", .code("http://localhost:8080"))
+        .li("Select the \(.code("HtmlVaporSupportExample")) target"),
+        .li("Build and run \(.code("cmd+R"))"),
+        .li("Open your browser to \(.code("http://localhost:8080"))")
       )
     )
   )
